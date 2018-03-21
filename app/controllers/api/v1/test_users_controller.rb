b7fb302abe4fbb45e93811d5e22ec9a08afcd601
imajoriri <imajo.srroni@gmail.com>
2018-03-21 15:55:45 +0900

    temp >> test

--- app/controllers/api/v1/test_users_controller.rb
+++ app/controllers/api/v1/test_users_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::TestUsersController < Api::V1::ApiController
     #respond_with current_resource_owner
     #TempUser.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
     #doorkeeper_token = "71cfd9279c682cee044f902f5dea293d8fa930fb7b828a48197dca3aac723eb4"
-    @temp_user = TempUser.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    @temp_user = TestUser.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
     if @temp_user
-      render json: {test: :not}
+      render json: {test: @temp_user}
     else 
       render json: {test: doorkeeper_token}
     end
